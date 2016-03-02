8f611a39e7de37b3187992d4ec534e2729e834fc
gkenji1986 <nakashig@oregonstate.edu>
2016-03-01 19:49:12 -0800

    Update poke_id.php
    http://web.engr.oregonstate.edu/~nakashig/main.php

--- databasesfinal/poke_id.php
+++ databasesfinal/poke_id.php
@@ -1,33 +1,21 @@
-		  
+<?php
+	$servername = "oniddb.cws.oregonstate.edu";
+	$username = "nakashig-db";
+	$password = "xXiNAmO8UFsRQc9d";
+	$dbname = "nakashig-db";
+		// Create connection
 
-
-		   <?php
-										$servername = "oniddb.cws.oregonstate.edu";
-										$username = "nakashig-db";
-										$password = "xXiNAmO8UFsRQc9d";
-										$dbname = "nakashig-db";
-										// Create connection
-									
-										$conn = new mysqli($servername, $username, $password, $dbname);
-										// Check connection
-										if ($conn->connect_error) 
-										{
-										  die("Connection failed: " . $conn->connect_error);
-										} 
-										$value1 = $_POST['value1'];
-										$value2 = $_POST['value2'];
-										$value3 = $_POST['value3'];
-										$sql = "INSERT INTO pokemon (poke_id,name,description) VALUES('$value1','$value2','$value3')";
-
-
-										$result = $conn->query($sql);
-										
-										
-    									// output data of each row
-    									while($row = $result->fetch_assoc()) {
-       									echo "id: " . $row["value1"]. " - Name: " . $row["value2"]. "Description: " . $row["value3"]. "<br>";
-   										 }
-   										
-										$conn->close();
-								
+	$id = $_POST['value1'];
+	$name = $_POST['value2'];
+	$desc = $_POST['value3'];
+		
+	$mysql = new mysqli($servername,$username, $password, $dbname);												
+	$stmt = $mysql->prepare('INSERT INTO pokemon (pokemon_id, name, description) VALUES (?, ?, ?)');
+	$stmt->bind_param('s', $id, $name, $desc);
+	$stmt->execute();				
+	$mysql->close();
+		
+	header("Location:main.php");
+    exit();
 ?>
+
