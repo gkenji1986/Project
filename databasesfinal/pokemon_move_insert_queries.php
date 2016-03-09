c4ba1993ed864ab138552cafbe8b228bc2de3539
carlin-james <carlinja@oregonstate.edu>
2016-03-08 21:53:32 -0500

    removal of comments

--- databasesfinal/pokemon_move_insert_queries.php
+++ databasesfinal/pokemon_move_insert_queries.php
@@ -1,24 +1,19 @@
 <?php
-//if it is to add a move to the list
 	if(isset($_POST['addMoveList']))
 	{
-		//set up server information
 		$servername = "oniddb.cws.oregonstate.edu";
 		$username = "nakashig-db";
 		$password = "xXiNAmO8UFsRQc9d";
 		$dbname = "nakashig-db";
-		//set all post information
+		// Create connection
 		$move_name = $_POST['name'];
 		$basedmg = $_POST['baseDamage'];
 		$power_p = $_POST['pp'];
-
 		$desc = $_POST['moveDesc'];
 		$type = $_POST['moveType'];
-		//set up sql connection
-		$mysql = new mysqli($servername, $username, $password, $dbname);
-		//sql SELECT statement												
+		
+		$mysql = new mysqli($servername, $username, $password, $dbname);												
 		$stmt = $mysql->prepare('SELECT type_id FROM types WHERE type_name = (?)');
-		//bind it, then execute it
 		$stmt->bind_param('s', $type);
 		$stmt->execute();
 		$stmt->bind_result($move_Type);
@@ -29,21 +24,18 @@
 		$stmt->bind_param('sssss', $move_name, $move_Type, $basedmg, $power_p, $desc);
 		
 		$stmt->execute();
-
 		$stmt->close();
-		//close the connection
+		
 		$mysql->close();
 	}
-//if the post is to add a pokemon move	
+	
 	if(isset($_POST['addPokeMove']))
 	{
-		//set up server information
 		$servername = "oniddb.cws.oregonstate.edu";
 		$username = "nakashig-db";
 		$password = "xXiNAmO8UFsRQc9d";
 		$dbname = "nakashig-db";
-		// Create connection 
-		//set up post information
+		// Create connection
 		$name = $_POST['pokeName'];
 		$move = $_POST['pokeMove'];
 		
@@ -53,17 +45,15 @@
 		$stmt->execute();
 		$stmt->bind_result($p_name);
 		$results = $stmt->fetch();
-		//set up sql with server information
-		$mysql2 = new mysqli($servername, $username, $password, $dbname);	
-		//get ready to send out the SELECT statement, based on values provided by the php page													
+		
+		$mysql2 = new mysqli($servername, $username, $password, $dbname);												
 		$stmt = $mysql2->prepare('SELECT move_id FROM moves WHERE move_name = (?)');
 		$stmt->bind_param('s', $move);
 		$stmt->execute();
 		$stmt->bind_result($p_move);
 		$results = $stmt->fetch();
-		//set up sql with server information
+		
 		$mysql3 = new mysqli($servername, $username, $password, $dbname);	
-		//get ready to send out the INSERT statement, based on values provided by the php page		
 		$stmt = $mysql3->prepare('INSERT INTO pokemon_moves (pid, mid) VALUES (?, ?)');
 		$stmt->bind_param('ss', $p_name, $p_move);
 
@@ -72,30 +62,29 @@
 		
 		$mysql->close();
 	}
-//if the post is to update a move information		
+	
 	if(isset($_POST['updateMove']))
 	{
-		//set up server information
 		$servername = "oniddb.cws.oregonstate.edu";
 		$username = "nakashig-db";
 		$password = "xXiNAmO8UFsRQc9d";
 		$dbname = "nakashig-db";
 			// Create connection
-		//set up all post information
 		$move = $_POST['updateMove'];
 		$damage = $_POST['updateDmg'];
 		$pp = $_POST['updatePp'];
-			//set up sql with server information
-		$mysql = new mysqli($servername,$username, $password, $dbname);	
-		//get ready to send out the UPDATE statement, based on values provided by the php page											
+			
+		$mysql = new mysqli($servername,$username, $password, $dbname);												
 		$stmt = $mysql->prepare('UPDATE moves SET move_base_damage = (?), move_pp = (?) WHERE move_name = (?)');
 		$stmt->bind_param('sss', $damage, $pp, $move);
-		$stmt->execute();	
-		//close			
+		$stmt->execute();				
 		$mysql->close();
 	}
 	
-
+	if(isset($_POST['addMoveList']))
+	{
+		
+	}
 	header("Location:insert_move.php");
     exit();
 ?>
